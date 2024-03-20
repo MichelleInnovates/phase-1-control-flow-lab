873b155bc572d14893b52b7824b785045f25cbd9
Michelle <azariahthesecond@gmail.com>
2024-03-21 01:05:54 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function scuberGreetingForFeet(x) {
+    // Write your code here!
+    if (x <= 400) {
+  
+      return 'This one is on me!';
+    }
+    else if(x > 400 && x <= 2000) {
+      return 'That will be twenty bucks.';
+    }
+    else if(x > 2000 && x <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    }
+   else if(x > 2500){
+      return 'No can do.';
+    }
+  
+  
+ 
+  function ternaryCheckCity(city) {
+    // Write your code here!
+    if (city === 'NYC'){
+      return "Ok, sounds good.";
+    }
+    else (city === 'Pittsburgh');{
+      return "No go.";
+    }
+    }
+  
+  
+ 
+  function switchOnCharmFromTip(tip) {
+    // Write your code here!
+    if (tip === 'generous') {
+      return 'Thank you so much.';
+    }
+    else if (tip === 'not as generous') {
+      return 'Thank you.';
+    }
+    else {
+      return 'Bye.';
+    }
+  }
+  
+  }
--- test/indexTest.js
+++ test/indexTest.js
@@ -21,11 +21,11 @@ describe('index.js', function () {
 
   describe('ternaryCheckCity()', function () {
     it('returns "Ok, sounds good." when the city is NYC', function () {
-      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
-    });
+      expect(ternaryCheckCity('NYC')).to.equal("Ok, sounds good.");
+          });
 
     it('should return "No go." if the destination city is not NYC', function () {
-      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+      expect(ternaryCheckCity('Pittsburgh')).to.equal("No go.");
     });
   });
 
